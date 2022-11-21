9ef692b38878352d9fcefc4143b7b4c4cdefeb7d
dedalovitor <dedalovitor@gmail.com>
2022-11-21 19:41:31 +0000

    uploading my code

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+
   document.querySelector("#btn").addEventListener("click", () => {
     document.querySelector("#the-excuse").innerHTML = generateExcuse();
   });
